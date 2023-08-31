9b42954c94c4fb22142966b97c8fcb552a07e20b
lzfgit-byte <1807409742@qq.com>
2023-08-31 18:54:07 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
-import locale from 'element-plus/dist/locale/zh-cn'; // element中文
+import locale from 'element-plus/es/locale/lang/zh-cn'; // element中文
 import App from '@/App.vue';
 import 'element-plus/dist/index.css';
 
 const app = createApp(App);
-app.use(ElementPlus).mount('#app');
+app.use(ElementPlus,{locale}).mount('#app');
