c56e57f696012a23c56dbaa679936658c4602364
lzfgit-byte <1807409742@qq.com>
2023-10-11 21:44:55 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,26 @@
   <ElButton :need-permission="false" @click="getXml">获取xml</ElButton>
   <ElButton :need-permission="false" @click="methods.setReadOnly(true)">查看模式</ElButton>
   <ElButton :need-permission="false" @click="methods.setReadOnly(false)">编辑模式</ElButton>
+  <ElButton :need-permission="false" @click="dialogReactive.show('')">导入xml</ElButton>
   <div style="width: 100%; height: 92vh">
     <PowerGraph ref="gRef" v-bind="props"></PowerGraph>
   </div>
+  <el-dialog v-model="dialogReactive.dialogVisible" title="xml" width="1000">
+    <el-input v-model="dialogReactive.textarea" :rows="20" type="textarea" />
+    <template #footer>
+      <ElButton @click="dialogReactive.confirm">确认</ElButton>
+    </template>
+  </el-dialog>
 </template>
 <script setup lang="ts">
-  import { ElButton, ElMessage } from 'element-plus';
-  import { ref } from 'vue';
+  import { ElButton } from 'element-plus';
+  import { reactive, ref } from 'vue';
   // @ts-expect-error
   import timeLine from './xml/timeLine.xml?raw';
-  import mx from './hook/useGraphFactory';
   import PowerGraph from '@/PowerGraph.vue';
   import usePowerGraph from '@/hook/usePowerGraph';
-  const { mxConstants } = mx;
-  let xml =
-    '<mxGraphModel><root><mxCell id="0"/><mxCell id="1" parent="0"/><mxCell id="kXvipDgzVxv_EnZ8P-GoQ" value="" style="rounded=0;whiteSpace=wrap;html=1;" vertex="1" parent="1"><mxGeometry x="120" y="120" width="120" height="60" as="geometry"/><Object tooltip="asdasd" as="businessData"/></mxCell></root></mxGraphModel>';
   const isSimpleModel = ref(false);
+
   const [gRef, props, methods] = usePowerGraph({
     isSimpleModel: isSimpleModel.value,
     onSidebarLoaded: () => {
@@ -25,8 +29,23 @@
       methods.setReadOnly(true);
     },
   });
+
+  const dialogReactive = reactive({
+    dialogVisible: false,
+    textarea: '',
+    confirm: () => {
+      methods.setXml(dialogReactive.textarea);
+      dialogReactive.dialogVisible = false;
+    },
+    show: (xml) => {
+      if (xml) {
+        dialogReactive.textarea = xml;
+      }
+      dialogReactive.dialogVisible = true;
+    },
+  });
   const getXml = () => {
-    ElMessage.info(methods.getXml());
+    dialogReactive.show(methods.getXml());
   };
 </script>
 
